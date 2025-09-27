98251fb05172609368072f53f999c17eea5d71f2
yetanotherchris <54912+yetanotherchris@users.noreply.github.com>
2025-09-27 15:35:37 +0000

    Update scoop and homebrew files to version 1.3.2

--- Formula/ask-llm.rb
+++ Formula/ask-llm.rb
@@ -1,33 +1,33 @@
 class AskLlm < Formula
   desc "Ask any large language model from your terminal via OpenAI-compatible APIs"
   homepage "https://github.com/yetanotherchris/ask-llm"
-  version "1.3.1"
+  version "1.3.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/ask-llm/releases/download/v1.3.1/askllm-v1.3.1-osx-arm64"
-      sha256 "058FE3D13824B4C691037D6B187805C52C9BBA0B07FBA983AD5F286DF17F5900"
+      url "https://github.com/yetanotherchris/ask-llm/releases/download/v1.3.2/askllm-v1.3.2-osx-arm64"
+      sha256 "61cf3349183efc2a27d828b9afed6f70e6771b2c350bed000a4a22cd67375deb"
     else
-      url "https://github.com/yetanotherchris/ask-llm/releases/download/v1.3.1/askllm-v1.3.1-osx-x64"
-      sha256 "F6452991009297D657CD899C75A775EEC0F8276099716AF1123ED44C2E665557"
+      url "https://github.com/yetanotherchris/ask-llm/releases/download/v1.3.2/askllm-v1.3.2-osx-x64"
+      sha256 "c6f884cb70997c9aa2d2f8fe4ea6e257892999efddbf73e702fb071645478be5"
     end
   end
 
   on_linux do
-    url "https://github.com/yetanotherchris/ask-llm/releases/download/v1.3.1/askllm-v1.3.1-linux-x64"
-    sha256 "7F4774E4DB353C84031BD9280C2C19D5AE559C9D4D69D2555958E34D301039E0"
+    url "https://github.com/yetanotherchris/ask-llm/releases/download/v1.3.2/askllm-v1.3.2-linux-x64"
+    sha256 "16308b44d4402f4e07bea3de92f715ebe1e29dba1bd1b2dae222a5c5516b77ef"
   end
 
   def install
     if OS.mac?
       if Hardware::CPU.arm?
-        bin.install "askllm-v1.3.1-osx-arm64" => "askllm"
+        bin.install "askllm-v1.3.2-osx-arm64" => "askllm"
       else
-        bin.install "askllm-v1.3.1-osx-x64" => "askllm"
+        bin.install "askllm-v1.3.2-osx-x64" => "askllm"
       end
     else
-      bin.install "askllm-v1.3.1-linux-x64" => "askllm"
+      bin.install "askllm-v1.3.2-linux-x64" => "askllm"
     end
   end
 
@@ -35,5 +35,6 @@ class AskLlm < Formula
     assert_match "USAGE:", shell_output("#{bin}/askllm --help")
   end
 end
+
 
 
